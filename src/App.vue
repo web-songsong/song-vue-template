accfe4779bf4036c24a757c9d2deec7e64131710
charmingsong <web_songsong@163.com>
2018-10-22 19:23:59 +0800

    0.0.7

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <hr>
+    <!-- 默认状态 -->
+    <song-switch />
+    <hr>
+    <!-- 禁用状态 -->
+    <song-switch :disabled="true" />
+    <hr>
+    <!-- 小状态 -->
+    <song-switch modulType="min" />
+    <hr>
+    <!-- 正常状态 -->
+    <song-switch modulType="switch" />
+    <hr>
+    <!-- 大状态 -->
+    <song-switch modulType="big" />
+    <hr>
+    <!-- 左、右（关闭、激活）有文字 -->
+    <song-switch activeText="关闭状态"
+                 inactiveText="激活状态"
+                 :textStyle="textStyle" />
+    <hr>
+    <!-- 配合change事件来改变文本 -->
+    <song-switch :inactiveText="text"
+                 @change="change" />
+    <hr>
+    <!-- 关闭背景颜色、激活背景颜色 -->
+    <song-switch inactiveColor="red"
+                 activeColor="yellow" />
+    <hr>
+    <!-- 是否有动画 -->
+    <song-switch :animate="false" />
+    <hr>
+    <!-- 滑动按钮颜色 -->
+    <song-switch toogleColor="pink" />
+    <hr>
+    <!-- 初始位置 -->
+    <song-switch :flag="true" />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import songSwitch from './components/Switch/index.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    songSwitch
+  },
+  data() {
+    return {
+      text: '关闭状态',
+      textStyle: {
+        color: 'yellow',
+        fontSize: '25px'
+      }
+    }
+  },
+  methods: {
+    change(flag) {
+      console.log('callback', flag)
+
+      this.text = flag ? '激活状态' : '关闭状态'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
