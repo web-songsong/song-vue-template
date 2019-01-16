1f1e6924196b3c7dc4772d3fa72c6789600b307b
charmingsong <web_songsong@163.com>
2019-01-17 01:51:20 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <svt-switch />
-
+    <svtSwitch />
   </div>
 </template>
 <style  scoped>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import path from 'path'
-import svt from 'song-vue-template'
-import 'song-vue-template/dist/svt.css'
 Vue.config.productionTip = false
-// const packages = require.context('../packages', true, /main\.vue$/)
-// packages.keys().forEach(item => {
-//   const name = path.join(item, '..')
-//   const componentConfig = packages(item)
-//   const components_name = `Svt${name[0].toUpperCase()}${name.slice(1)}`
-//   console.log(components_name)
-//   Vue.component(components_name, componentConfig.default || componentConfig)
-// })
-// import svt from '../dist/svt.common.js'
-// Vue.use(svt)
-Vue.use(svt)
-console.log(svt)
+const packages = require.context('../packages', true, /main\.vue$/)
+packages.keys().forEach(item => {
+  const name = path.join(item, '..')
+  const componentConfig = packages(item)
+  const components_name = `Svt${name[0].toUpperCase()}${name.slice(1)}`
+  componentConfig.default
+    ? (componentConfig.default.name = components_name)
+    : (componentConfig.name = components_name)
+  Vue.component(components_name, componentConfig.default || componentConfig)
+})
 new Vue({
   render: h => h(App)
 }).$mount('#app')
