da3cf26824ea041aa7b0603a3112a2a1058fdca3
charmingsong <web_songsong@163.com>
2019-03-14 15:18:51 +0800

    npm files update

--- examples/entry.js
+++ examples/entry.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import app from './app'
 import router from './router'
 import 'lib-flexible/flexible.js'
-import CreateAPI from 'vue-create-api'
 import Svt from 'main'
-import { Toast } from 'main'
-import Hello from './Hello.vue'
 
 Vue.use(Svt)
 new Vue({
